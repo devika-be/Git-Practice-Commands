2180b6d679bd14982b015fe5f70c8fb7e8cbe1dd
“Devika <devika.be17gmail.com>
2025-09-03 15:18:26 +0530

    feat: Add initial files for my first feature

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  // your firebase credentials go here
+  apiKey: "AIzaSyBp3pP0GeWs3FBahxRWleSTMAnEZZ2tqZI",
+  authDomain: "instagram-clone-86cf5.firebaseapp.com",
+  databaseURL: "https://instagram-clone-86cf5.firebaseio.com",
+  projectId: "instagram-clone-86cf5",
+  storageBucket: "instagram-clone-86cf5.appspot.com",
+  messagingSenderId: "1037460570779",
+  appId: "1:1037460570779:web:9175c38a0d1039c0b2bc3e",
+  measurementId: "G-1L0D05LTSH"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
